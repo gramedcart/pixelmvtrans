863fc879acffc5331775c177ba6a0273cbf7c125
gramedcart <anbulbay@gmail.com>
2022-03-11 01:36:48 +0900

    1.0.1

--- src/lib/projectBuilder.js
+++ src/lib/projectBuilder.js
@@ -7,70 +7,82 @@ exports.initProject = void 0;
 const path_1 = __importDefault(require("path"));
 const fs_1 = __importDefault(require("fs"));
 const child_process_1 = require("child_process");
-const iconv_lite_1 = __importDefault(require("iconv-lite"));
 const electron_1 = require("electron");
 const perf_hooks_1 = require("perf_hooks");
 const mainWindow_1 = require("./mainWindow");
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-function findKey(dir) {
-    const infoJson = path_1.default.join(dir, 'data', 'info.json');
-    const d = JSON.parse(fs_1.default.readFileSync(infoJson, 'utf-8')).key;
-    return new Uint8Array(Buffer.from(d, 'base64'));
-}
 async function initProject(sourceDir, gameDir) {
-    function decrypt(tempStorage) {
-        return new Promise((resolve) => {
-            try {
-                const d = (fs_1.default.readFileSync(projectDir, 'utf-8')).replaceAll('\u0000', '');
-                JSON.parse(d);
-                console.log('already decrypted');
-                fs_1.default.writeFileSync(tempProject, d);
-                resolve();
-            }
-            catch (error) {
+    try {
+        function decrypt(tempStorage) {
+            return new Promise((resolve) => {
+                try {
+                    const d = (fs_1.default.readFileSync(projectDir, 'utf-8')).replaceAll('\u0000', '');
+                    JSON.parse(d);
+                    console.log('already decrypted');
+                    fs_1.default.writeFileSync(tempProject, d);
+                    resolve();
+                }
+                catch (error) {
+                    const pypy = `${path_1.default.join(globalThis.oPath, 'ext', 'python', 'pypy', 'pypy.exe')}`;
+                    const de = (0, child_process_1.spawn)(pypy, [
+                        path_1.default.join(globalThis.oPath, 'ext', 'python', 'unpack2.py'), `${projectDir}`, `${tempProject}`, `${key}`
+                    ]);
+                    de.stdout.on('data', (d) => {
+                        console.log(d.toString('ascii'));
+                    });
+                    de.on("exit", (msg) => {
+                        resolve();
+                    });
+                }
+            });
+        }
+        function getKey(Unkey) {
+            return new Promise((resolve) => {
                 const pypy = `${path_1.default.join(globalThis.oPath, 'ext', 'python', 'pypy', 'pypy.exe')}`;
                 const de = (0, child_process_1.spawn)(pypy, [
-                    path_1.default.join(globalThis.oPath, 'ext', 'python', 'unpack2.py'), `${projectDir}`, `${tempProject}`, `${key}`
+                    path_1.default.join(globalThis.oPath, 'ext', 'python', 'unpack.py'), `${Unkey}`
                 ]);
-                de.on("exit", (msg) => {
-                    resolve();
+                de.stdout.on("data", (str) => {
+                    str = str.subarray(0, str.indexOf(0));
+                    const b64a = str.toString('ascii');
+                    console.log(b64a);
+                    resolve(Buffer.from(b64a, 'base64'));
                 });
-            }
-        });
-    }
-    function getKey(Unkey) {
-        return new Promise((resolve) => {
-            const pypy = `${path_1.default.join(globalThis.oPath, 'ext', 'python', 'pypy', 'pypy.exe')}`;
-            const de = (0, child_process_1.spawn)(pypy, [
-                path_1.default.join(globalThis.oPath, 'ext', 'python', 'unpack.py'), `${Unkey}`
-            ]);
-            de.stdout.on("data", (str) => {
-                str = str.subarray(0, str.indexOf(0));
-                const b64a = str.toString('ascii');
-                resolve(b64a);
             });
-        });
+        }
+        const nau = perf_hooks_1.performance.now();
+        const tempStorage = (path_1.default.join(electron_1.app.getPath("appData"), electron_1.app.getName()));
+        const dir = path_1.default.join(sourceDir, 'Resources');
+        console.log('init');
+        const projectDir = path_1.default.join(dir, "data", "project.json");
+        const tempProject = path_1.default.join(tempStorage, 'prj.json');
+        if (fs_1.default.existsSync(tempProject)) {
+            fs_1.default.rmSync(tempProject);
+        }
+        const Unkey = JSON.parse(fs_1.default.readFileSync(path_1.default.join(dir, "data", "info.json"), 'utf-8')).key;
+        const key2 = await getKey(Unkey);
+        console.log(`|${key2.toString('ascii')}|`);
+        const key = key2.toString('base64');
+        console.log(key);
+        console.log(tempProject);
+        console.log(projectDir);
+        await decrypt(tempStorage);
+        globalThis.sourceDir = sourceDir;
+        const dat = JSON.parse((fs_1.default.readFileSync(tempProject, 'utf-8')).replaceAll('\u0000', ''));
+        globalThis.project = dat;
+        console.log('Decrypt Success');
+        if (perf_hooks_1.performance.now() - nau < 1000) {
+            console.log('waiting..');
+            await sleep(1000);
+        }
+        mainWindow_1.mwindow.webContents.send("open", { project: globalThis.project });
+        console.log('open');
     }
-    const nau = perf_hooks_1.performance.now();
-    const tempStorage = (path_1.default.join(electron_1.app.getPath("appData"), electron_1.app.getName()));
-    const dir = path_1.default.join(sourceDir, 'Resources');
-    console.log('init');
-    const projectDir = path_1.default.join(dir, "data", "project.json");
-    const tempProject = path_1.default.join(tempStorage, 'prj.json');
-    const Unkey = JSON.parse(fs_1.default.readFileSync(path_1.default.join(dir, "data", "info.json"), 'utf-8')).key;
-    const key = iconv_lite_1.default.encode(await getKey(Unkey), "utf-8").toString('base64');
-    await decrypt(tempStorage);
-    globalThis.sourceDir = sourceDir;
-    const dat = JSON.parse((fs_1.default.readFileSync(tempProject, 'utf-8')).replaceAll('\u0000', ''));
-    globalThis.project = dat;
-    console.log('Decrypt Success');
-    if (perf_hooks_1.performance.now() - nau < 1000) {
-        console.log('waiting..');
-        await sleep(1000);
+    catch (error) {
+        console.log(error);
+        mainWindow_1.mwindow.webContents.send("warn", "오류가 발생했습니다");
     }
-    mainWindow_1.mwindow.webContents.send("open", { project: globalThis.project });
-    console.log('open');
 }
 exports.initProject = initProject;
--- src/lib/projectBuilder.ts
+++ src/lib/projectBuilder.ts
@@ -10,62 +10,75 @@ function sleep(ms:number) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function findKey(dir:string){
-    const infoJson = path.join(dir, 'data', 'info.json')
-    const d:string = JSON.parse(fs.readFileSync(infoJson, 'utf-8')).key
-    return new Uint8Array(Buffer.from(d, 'base64'))
-}
-
 export async function initProject(sourceDir:string, gameDir:string){
-    function decrypt(tempStorage:string){
-        return new Promise<void>((resolve) => {
-            try {
-                const d = (fs.readFileSync(projectDir, 'utf-8')).replaceAll('\u0000', '')
-                JSON.parse(d)
-                console.log('already decrypted')
-                fs.writeFileSync(tempProject, d)
-                resolve()
-            } catch (error) {
+    try {
+        function decrypt(tempStorage:string){
+            return new Promise<void>((resolve) => {
+                try {
+                    const d = (fs.readFileSync(projectDir, 'utf-8')).replaceAll('\u0000', '')
+                    JSON.parse(d)
+                    console.log('already decrypted')
+                    fs.writeFileSync(tempProject, d)
+                    resolve()
+                } catch (error) {
+                    const pypy = `${path.join(globalThis.oPath, 'ext', 'python', 'pypy', 'pypy.exe')}`
+                    const de = spawn(pypy, [
+                        path.join(globalThis.oPath, 'ext', 'python', 'unpack2.py'), `${projectDir}`, `${tempProject}`, `${key}`
+                    ])
+    
+                    de.stdout.on('data', (d:Buffer) => {
+                        console.log(d.toString('ascii'))
+                    })
+                    de.on("exit", (msg) => {
+                        resolve()
+                    })
+                }
+            })
+        }
+        function getKey(Unkey: string){
+            return new Promise<Buffer>((resolve) => {
                 const pypy = `${path.join(globalThis.oPath, 'ext', 'python', 'pypy', 'pypy.exe')}`
                 const de = spawn(pypy, [
-                    path.join(globalThis.oPath, 'ext', 'python', 'unpack2.py'), `${projectDir}`, `${tempProject}`, `${key}`
+                    path.join(globalThis.oPath, 'ext', 'python', 'unpack.py'), `${Unkey}`
                 ])
-                de.on("exit", (msg) => {
-                    resolve()
+                de.stdout.on("data", (str:Buffer) => {
+                    str = str.subarray(0, str.indexOf(0))
+                    const b64a = str.toString('ascii')
+                    console.log(b64a)
+                    resolve(Buffer.from(b64a, 'base64'))
                 })
-            }
-        })
-    }
-    function getKey(Unkey: string){
-        return new Promise<string>((resolve) => {
-            const pypy = `${path.join(globalThis.oPath, 'ext', 'python', 'pypy', 'pypy.exe')}`
-            const de = spawn(pypy, [
-                path.join(globalThis.oPath, 'ext', 'python', 'unpack.py'), `${Unkey}`
-            ])
-            de.stdout.on("data", (str:Buffer) => {
-                str = str.subarray(0, str.indexOf(0))
-                const b64a = str.toString('ascii')
-                resolve(b64a)
             })
-        })
+        }
+        const nau = performance.now()
+        const tempStorage = (path.join(app.getPath("appData"), app.getName()))
+        const dir = path.join(sourceDir, 'Resources')
+        console.log('init')
+        const projectDir = path.join(dir, "data", "project.json")
+        const tempProject = path.join(tempStorage, 'prj.json')
+        if(fs.existsSync(tempProject)){
+            fs.rmSync(tempProject)
+        }
+        const Unkey:string = JSON.parse(fs.readFileSync(path.join(dir, "data", "info.json"), 'utf-8')).key
+        const key2 = await getKey(Unkey)
+        console.log(`|${key2.toString('ascii')}|`)
+        const key = key2.toString('base64')
+        console.log(key)
+    
+        console.log(tempProject)
+        console.log(projectDir)
+        await decrypt(tempStorage)
+        globalThis.sourceDir = sourceDir
+        const dat:ProjectJSON = JSON.parse((fs.readFileSync(tempProject, 'utf-8')).replaceAll('\u0000', ''))
+        globalThis.project = dat
+        console.log('Decrypt Success')
+        if(performance.now() - nau < 1000){
+            console.log('waiting..')
+            await sleep(1000)
+        }
+        mwindow.webContents.send("open", {project: globalThis.project})
+        console.log('open');   
+    } catch (error) {
+        console.log(error)
+        mwindow.webContents.send("warn", "오류가 발생했습니다")
     }
-    const nau = performance.now()
-    const tempStorage = (path.join(app.getPath("appData"), app.getName()))
-    const dir = path.join(sourceDir, 'Resources')
-    console.log('init')
-    const projectDir = path.join(dir, "data", "project.json")
-    const tempProject = path.join(tempStorage, 'prj.json')
-    const Unkey:string = JSON.parse(fs.readFileSync(path.join(dir, "data", "info.json"), 'utf-8')).key
-    const key = iconv.encode(await getKey(Unkey), "utf-8").toString('base64')
-    await decrypt(tempStorage)
-    globalThis.sourceDir = sourceDir
-    const dat:ProjectJSON = JSON.parse((fs.readFileSync(tempProject, 'utf-8')).replaceAll('\u0000', ''))
-    globalThis.project = dat
-    console.log('Decrypt Success')
-    if(performance.now() - nau < 1000){
-        console.log('waiting..')
-        await sleep(1000)
-    }
-    mwindow.webContents.send("open", {project: globalThis.project})
-    console.log('open');
 }
